e143e4ec08e51b0b4256a213ffe21bf7de5bc294
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-03-14 16:20:02 +0300

    change logo header

--- client/script.js
+++ client/script.js
@@ -37,3 +37,35 @@ if (animItems.length > 0) {
   }, 300);
 }
 
+
+
+let total_pics_num = 4; // колличество изображений
+let interval = 5000;    // задержка между изображениями
+let time_out = 1;       // задержка смены изображений
+let i = 0;
+let timeout;
+let opacity = 100;
+function fade_to_next() {
+  opacity--;
+  let k = i + 1;
+  let image_now = 'image_' + i;
+  if (i == total_pics_num) k = 1;
+  let image_next = 'image_' + k;
+  const imageNow = document.getElementById(image_now);
+  imageNow.style.opacity = opacity / 100;
+  imageNow.style.filter = 'alpha(opacity=' + opacity + ')';
+  document.getElementById(image_next).style.opacity = (100 - opacity) / 100;
+  document.getElementById(image_next).style.filter = 'alpha(opacity=' + (100 - opacity) + ')';
+  timeout = setTimeout("fade_to_next()", time_out);
+  if (opacity == 1) {
+    opacity = 100;
+    clearTimeout(timeout);
+  }
+}
+setInterval(
+  function () {
+    i++;
+    if (i > total_pics_num) i = 1;
+    fade_to_next();
+  }, interval
+);
